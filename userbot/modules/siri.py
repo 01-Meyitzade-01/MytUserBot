1d349c8b694a85666b97fbccc665f2b077f58a39
01-Meyitzade-01 <83854378+01-Meyitzade-01@users.noreply.github.com>
2022-04-09 11:24:05 +0300

    Update siri.py

--- userbot/modules/siri.py
+++ userbot/modules/siri.py
@@ -7,7 +7,7 @@ from userbot.events import register
 @register(outgoing=True,pattern="^.[Ss]iri")
 
 async def komut(event):
-        await event.edit("**Alışkanlık İşte**\n __Botumuzun İsmi Değişti Unuttun mu Siri Yazmak Yerine__ `.owen` __Yazmalısın.__\n \n**Gerekli Açıklama:** t.me/OwenUserBot/65 \n UserBot Kanalı: @OwenUserBot\nPlugin Kanalı: @OwenPlugin")
+        await event.edit("**Alışkanlık İşte**\n __Botumuzun İsmi Değişti Unuttun mu siri Yazmak Yerine__ `.owen` __Yazmalısın.__\n \n**Gerekli Açıklama:** t.me/Hiraset/65 \n UserBot Kanalı: @MytUserBot\nPlugin Kanalı: @OwenPlugin")
 
 @register(outgoing=True,pattern="^.[Ee]pic")
 
