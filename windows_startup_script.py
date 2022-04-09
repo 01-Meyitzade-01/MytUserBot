dba0e93475ceb9dd4d9f16af54fb516d9169dda9
01-Meyitzade-01 <83854378+01-Meyitzade-01@users.noreply.github.com>
2022-04-09 05:45:45 +0300

    Update windows_startup_script.py

--- windows_startup_script.py
+++ windows_startup_script.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient
-API_KEY="Bunu buraya yazın"
-API_HASH="Bunu buraya yazın"
+API_KEY="2930013"
+API_HASH="7cab92dcd979add511b79d693775e17d"
 #my.telegram.org adresinden alın
 bot = TelegramClient('userbot',API_KEY,API_HASH)
 bot.start()
