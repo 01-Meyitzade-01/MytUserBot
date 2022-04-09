9177b5dd8a2dbf68e54d2e3af872e8c68b9ebeb6
01-Meyitzade-01 <83854378+01-Meyitzade-01@users.noreply.github.com>
2022-04-09 12:32:45 +0300

    Update gps.py

--- userbot/modules/gps.py
+++ userbot/modules/gps.py
@@ -21,7 +21,7 @@ async def gps(event):
     input_str = event.pattern_match.group(1)
 
     if not input_str:
-        return await event.edit("`Owenye Bir Bölgenin İsmini Vermezsen Bulamaz Ki.`")
+        return await event.edit("`Myt'ye Bir Bölgenin İsmini Vermezsen Bulamaz Ki.`")
 
     await event.edit("**Buluyorum...**")
 
